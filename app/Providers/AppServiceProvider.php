f5aa5ef3c9a1a21e534d766a18cb6dfc13c3247a
Esther Babaoye <estherbabaoye@gmail.com>
2025-07-31 21:46:49 +0100

    bug fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,21 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Bootstrap any application services.
      */
-    public function register(): void
+    public function boot()
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
-     * Bootstrap any application services.
+     * Register any application services.
      */
-    public function boot(): void
+    public function register()
     {
         //
     }
