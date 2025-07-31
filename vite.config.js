becbad75cb8855f7a01394b1c45d91e7514aec9f
Esther Babaoye <estherbabaoye@gmail.com>
2025-07-31 21:57:07 +0100

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
     }),
     react(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/build/' : '/',
+  base: '/build/', // ✅ force correct asset prefix for production
 })
